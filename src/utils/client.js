1f67ba0af4e995810d378fb0730d45787e930986
Analyticsfox1 <bmodi@teracrunch.com>
2019-09-12 18:34:11 +0530

    change API url

--- src/utils/client.js
+++ src/utils/client.js
@@ -1,7 +1,7 @@
 var axios = require('axios');
 
 var axiosInstance = axios.create({
-  baseURL: 'http://13.126.20.61:8080',
+  baseURL: 'https://13.126.20.61:8080',
 });
 
 module.exports = axiosInstance;
